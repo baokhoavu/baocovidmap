b122a58ffd8dd263614fd730d474d524a0e79af1
baokhoavu <baokhoadinhvu@gmail.com>
2026-01-16 12:57:49 -0800

    Further fix for the map

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,19 +1,17 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
+
 export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
-    #root {
-        width: 100%;
-        height: 100%;
-    }
-
-    body {
+    html, body, #root {
         width: 100vw;
+        min-width: 100vw;
         height: 100vh;
+        min-height: 100vh;
         margin: 0;
         padding: 0;
         overflow: hidden;
